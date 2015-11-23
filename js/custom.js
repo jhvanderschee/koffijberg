8205e048423c1098290035feff7609e78d3358b8
Joost van der Schee <jhvanderschee@gmail.com>
2015-11-23 15:19:22 +0100

    update

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,47 @@
-function showslide(el) {
-	el.delay(300).animate( { height: "toggle" }, 1000, 'swing').delay(7000).fadeOut(300, function() {
-	if(el.next().is('article')) showslide(el.next());
-	else showslide($('.highlight article').first());
-	});
+function showslide(el,now) {
+	if (now) {
+		el.show().delay(7000).fadeOut(300, function() {
+		if(el.next().is('article')) showslide(el.next(),false);
+		else showslide($('.highlight article').first(),false);
+	    });
+	}
+	else {
+		el.delay(300).animate( { height: "toggle" }, 1000, 'swing').delay(7000).fadeOut(300, function() {
+		if(el.next().is('article')) showslide(el.next(),false);
+		else showslide($('.highlight article').first(),false);
+	    });
+	}
+
 }
+function get_cookie(Name) {
+    var search = Name + "="
+    var returnvalue = "";
+    if (document.cookie.length > 0) {
+        offset = document.cookie.indexOf(search)
+        // if cookie exists
+        if (offset != -1) { 
+            offset += search.length
+            // set index of beginning of value
+            end = document.cookie.indexOf(";", offset);
+            // set index of end of cookie value
+            if (end == -1) end = document.cookie.length;
+            returnvalue=unescape(document.cookie.substring(offset, end))
+        }
+    }
+    return returnvalue;
+}
+function set_cookie(what){
+    document.cookie="splashshown="+what
+}
+
 
 $( document ).ready(function() {
 	var iOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
 	if(iOS) $('body').addClass('iOS');
 	
+	if (get_cookie("splashshown")=="") $('#splash').show();
+	set_cookie('true');
+
     if(window.location.hash) {
     	var hash = window.location.hash.substring(1);
     	var offset = $('#'+hash).offset();
@@ -25,7 +58,7 @@ $( document ).ready(function() {
 	  else $( ".arrow" ).show( );
 	});
 	
-	showslide($('.highlight article').first());
+	showslide($('.highlight article').first(),true);
 	
 	$('a').click(function(){
 		el = $.attr(this, 'href').replace('/','');
@@ -36,4 +69,7 @@ $( document ).ready(function() {
 	});
 
 	$('#splash').delay(1500).fadeOut();
+    $('#review').css('left', '-'+$('#review').width()+'px');
+    $('#review').css('visibility','visible'); 
+	$('#review').animate({"left": '+='+$('#review').width()},1500);
 });
